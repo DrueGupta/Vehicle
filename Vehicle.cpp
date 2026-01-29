b8bcd93fbfdbc2cd265dacbf8d7c4fcaf9c082a1
Drue Gupta <1031762@my.scccd.edu>
2026-01-29 15:04:52 -0800

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include "Vehicle.h"
+using namespace std;
 
+double Vehicle::getX()
+{
+	return x;
+}
+
+double Vehicle::getY()
+{
+	return y;
+}
+
+double Vehicle::getOdometer()
+{
+	return odometer;
+}
+
+void Vehicle::moveUp(double y)
+{
+	this->y += y;
+	odometer += y;
+}
+
+void Vehicle::moveDown(double y)
+{
+	this->y -= y;
+	odometer += y;
+}
+
+void Vehicle::moveRight(double x)
+{
+	this->x += x;
+	odometer += x;
+}
+
+void Vehicle::moveLeft(double x)
+{
+	this->x -= x;
+	odometer += x;
+}
+
+void Vehicle::display()
+{
+	cout << x << " " << y << " " << odometer << endl;
+}
